d0eb947027cc567e8b6e2262a2316fcb3f70b656
Joel Byomuhangi <jbyomuhangi@gmail.com>
2025-06-28 21:54:19 +0800

    Created models

--- my_project/api/admin.py
+++ my_project/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, Product, Order, OrderItem
+
+
+admin.site.register(User)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_extensions",
     "api",
 ]
 
@@ -121,3 +122,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Set custom user model
+AUTH_USER_MODEL = "api.User"
