e58ce7a272a93c36ca387b72e2f56aa1fa45c664
Joel Byomuhangi <jbyomuhangi@gmail.com>
2025-06-29 10:44:39 +0800

    Added some more stuff using model serializers

--- my_project/api/serializers.py
+++ my_project/api/serializers.py
@@ -20,14 +20,24 @@ class ProductSerializer(serializers.ModelSerializer):
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
+    product_name = serializers.CharField(source="product.name", read_only=True)
+    product_price = serializers.DecimalField(
+        source="product.price", max_digits=10, decimal_places=2, read_only=True
+    )
+
     class Meta:
         model = OrderItem
-        fields = ["id", "product", "quantity"]
+        fields = ["id", "quantity", "product_name", "product_price", "item_subtotal"]
 
 
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField()
+
+    def get_total_price(self, obj):
+        order_items = obj.items.all()
+        return sum(order_item.item_subtotal for order_item in order_items)
 
     class Meta:
         model = Order
-        fields = ["id", "user", "status", "created_at", "items"]
+        fields = ["id", "user", "status", "created_at", "items", "total_price"]
